4953a870b22d082ab6144990c4d1f25e437d48c6
Malcolm Jefferson <malcolmj0415@gmail.com>
2024-11-28 15:08:37 -0800

    Adding initial styling

--- code-snapshots/02-essentials/01-starting-project/src/app/header.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/header.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    standalone: true
+    standalone: true,
+    styleUrl: './header.component.css'
 })
 
 export class HeaderComponent {}
