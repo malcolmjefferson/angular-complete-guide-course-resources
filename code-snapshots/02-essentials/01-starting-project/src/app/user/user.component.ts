198ef02ee4f6802048cf89ea42a1344134506a4c
malcolmjefferson <malcolmj0415@gmail.com>
2024-11-29 18:27:43 -0800

    Added output function onSelectUser output emitter

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Input } from '@angular/core';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,12 +10,14 @@ export class UserComponent {
 
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  @Input({required: true}) id!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
   
   onSelectUser() {
-    
+    this.select.emit(this.id);
   }
 }
