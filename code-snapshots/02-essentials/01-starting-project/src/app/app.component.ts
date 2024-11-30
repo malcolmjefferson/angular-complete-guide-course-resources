44f98be796fe5b65135ea95fba524396b3fac7d3
malcolmjefferson <malcolmj0415@gmail.com>
2024-11-29 18:53:05 -0800

    Added tasks component initial input. Can see selected user

--- code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { TasksComponent } from './tasks/tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUser: string | undefined = ''
 
   onSelectUser(id: string) {
     console.log('Selected user with id ' + id);
+    this.selectedUser = DUMMY_USERS.find((e) => e.id == id)?.name
   }
 
 }
