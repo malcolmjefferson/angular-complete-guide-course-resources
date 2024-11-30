2d0686a22565c4427643df928c08fd760cef94e4
malcolmjefferson <malcolmj0415@gmail.com>
2024-11-29 18:40:43 -0800

    Created initial Tasks component

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -11,7 +11,7 @@ export class UserComponent {
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
   @Input({required: true}) id!: string;
-  @Output() select = new EventEmitter<string>();
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
